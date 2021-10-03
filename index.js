286c8dd2c8de6cc07fa84134c0a544f582df2fd5
unknown <arian.angoma@tecsup.edu.pe>
2021-10-03 00:56:49 -0500

    Primeras configuraciones de express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hola asdad');
+const express = require('express');
+
+/* Servidor de express */
+const app = express();
+
+/* Rutas */
+app.get('/', (req, res) => {
+    res.json({
+        ok: true
+    });
+});
+
+/* Escuchar peticiones */
+app.listen(4000, () => {
+    console.log(`Servidor corriendo en puerto ${4000}`);
+});
